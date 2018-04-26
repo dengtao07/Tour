1fb6cdf0dc94935b01f97a9547d3d00f8b227317
dengtao <dtqwe@qq.com>
2018-04-26 21:39:36 +0800

    swiper init

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="wrapper">
+  <swiper :options="swiperOption">
+    <!-- slides -->
+    <swiper-slide>
+      <img class="swiper-image" src="http://img1.qunarzz.com/piao/fusion/1804/8b/f7f0585bbb539202.jpg_640x200_c4096429.jpg" />
+    </swiper-slide>
+    <swiper-slide>
+      <img class="swiper-image" src="http://img1.qunarzz.com/piao/fusion/1804/c7/15d167c86f961a02.jpg_640x200_e9d5a5ff.jpg" />
+    </swiper-slide>
+    <!-- Optional controls -->
+    <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+</div>
+</template>
+
+<script>
+export default {
+  neme: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {}
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 31.25%
+    .swiper-image
+      width: 100%
+</style>
