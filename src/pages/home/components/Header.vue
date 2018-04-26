e0fd6c0c53fc1712b4603928c353f29ef1c4387b
dengtao <dtqwe@qq.com>
2018-04-26 15:19:10 +0800

    Home header complete

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont arrow-icon">&#xe600;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/variables.styl'
+  .header
+    display: flex
+    height: .86rem
+    line-height: .86rem
+    background-color:  $commom_bgColor
+    color : #fff
+    .header-left
+      width: .64rem
+      .back-icon
+        text-align: center
+        font-size: .2rem
+    .header-input
+      flex: 1
+      height: .64rem
+      line-height: .64rem
+      background-color: #fff
+      border-radius: .1rem
+      margin-top: .12rem
+      margin-left: .2rem
+      color: #ccc
+      padding-left: .2rem
+    .header-right
+      width: 1.24rem
+      text-align: center
+      .arrow-icon
+        margin-lefrt: -.04rem
+        font-size: .2rem
+</style>
