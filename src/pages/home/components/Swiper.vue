a5e141e6c76d8e30e458c7f739cac24cd45d6eda
dengtao <dtqwe@qq.com>
2018-04-27 15:26:05 +0800

    swiper finished

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -2,11 +2,8 @@
 <div class="wrapper">
   <swiper :options="swiperOption">
     <!-- slides -->
-    <swiper-slide>
-      <img class="swiper-image" src="http://img1.qunarzz.com/piao/fusion/1804/8b/f7f0585bbb539202.jpg_640x200_c4096429.jpg" />
-    </swiper-slide>
-    <swiper-slide>
-      <img class="swiper-image" src="http://img1.qunarzz.com/piao/fusion/1804/c7/15d167c86f961a02.jpg_640x200_e9d5a5ff.jpg" />
+    <swiper-slide v-for="item of swiperList" :key="item.id">
+      <img class="swiper-image" :src="item.imgurl">
     </swiper-slide>
     <!-- Optional controls -->
     <div class="swiper-pagination"  slot="pagination"></div>
@@ -19,18 +16,31 @@ export default {
   neme: 'HomeSwiper',
   data () {
     return {
-      swiperOption: {}
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgurl: 'http://img1.qunarzz.com/piao/fusion/1804/8b/f7f0585bbb539202.jpg_640x200_c4096429.jpg'
+      }, {
+        id: '0002',
+        imgurl: 'http://img1.qunarzz.com/piao/fusion/1804/c7/15d167c86f961a02.jpg_640x200_e9d5a5ff.jpg'
+      }]
     }
   }
 }
 </script>
 
 <style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+      background-color: #fff !important
   .wrapper
     overflow: hidden
     width: 100%
     height: 0
     padding-bottom: 31.25%
+    background-color: #eee
     .swiper-image
       width: 100%
 </style>
