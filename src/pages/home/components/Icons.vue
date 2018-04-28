e94e0166c554126ad4729b90d4a1bc5e614c7c9e
dengtao <dtqwe@qq.com>
2018-04-28 10:13:33 +0800

    icon

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="wraper">
+    <div class="icon">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default{
+  name: 'HomeIcons'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wraper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 50%
+    background-color: #eee
+    .icon
+      float: left
+      width: 25% //基于父元素的宽度
+      padding-bottom: 25% //基于父元素的宽度
+      background-color: green
+</style>
