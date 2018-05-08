cf622ed8f0793fb7d186fa113e90524608467a6e
dengtao <dtqwe@qq.com>
2018-05-08 10:29:56 +0800

    city router

---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="header">
+    城市选择
+    <router-link to="/">
+      <div class="iconfont header-back">&#xe624;</div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/variables.styl'
+  .header
+    position: relative
+    overflow: hidden
+    height: $header_height
+    line-height: $header_height
+    text-align: center
+    color: #fff
+    background: $commom_bgColor
+    font-size: .3rem
+    .header-back
+      width: .5rem
+      position: absolute
+      top: 0
+      left: 0
+      color: #fff
+</style>
