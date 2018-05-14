52bfd851d477eae84ad8bbc68882ad56a37fa249
dengtao <dtqwe@qq.com>
2018-05-14 11:15:01 +0800

    city search completed

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,12 +1,83 @@
 <template>
-  <div class="search">
-    <input class="searchInput" type="text" placeholder="输入城市名或拼音">
+  <div>
+    <div class="search">
+      <input
+        class="searchInput"
+        type="text"
+        placeholder="输入城市名或拼音"
+        v-model="keyword"
+      >
+    </div>
+    <div
+      class="search-content"
+      ref="search"
+      v-show="keyword"
+    >
+      <ul>
+        <li
+          class="search-item border-bottom"
+          v-for="item in list"
+          :key="item.id"
+        >
+          {{item.name}}
+        </li>
+        <!-- 把逻辑运算都放到js代码中 -->
+        <li
+          v-show="hasNoData"
+          class="search-item border-bottom"
+        >
+          没有找到匹配数据
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import Bscroll from 'better-scroll'
 export default {
-  name: 'CitySearch'
+  name: 'CitySearch',
+  props: {
+    cities: Object
+  },
+  data () {
+    return {
+      keyword: '',
+      list: [],
+      timer: null
+    }
+  },
+  computed: {
+    hasNoData () {
+      return !this.list.length
+    }
+  },
+  watch: {
+    keyword () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      if (!this.keyword) {
+        this.list = []
+      } else {
+        this.timer = setTimeout(() => {
+          const result = []
+          for (let i in this.cities) {
+            this.cities[i].forEach((item) => {
+              if (item.spell.indexOf(this.keyword) > -1 ||
+                  item.name.indexOf(this.keyword) > -1) {
+                result.push(item)
+              }
+            })
+          }
+          this.list = result
+        }, 100)
+      }
+    }
+  },
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.search)
+  }
 }
 </script>
 
@@ -25,4 +96,18 @@ export default {
       text-align: center
       color: #666
       padding: 0 .2rem
+  .search-content
+    z-index: 1
+    overflow: hidden
+    position: absolute
+    top: 1.58rem
+    left: 0
+    right: 0
+    bottom: 0
+    background: #eee
+    .search-item
+      background: #ffffff
+      color: #666
+      padding-left: .2rem
+      line-height: .62rem
 </style>
